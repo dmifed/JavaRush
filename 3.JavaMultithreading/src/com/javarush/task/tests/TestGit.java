2e1f786e854af08164036e0a85957f40d3aec882
Dmitriy Fedorov <dmitriy_fedorov@list.ru>
2017-07-26 21:44:45 +0400

    my Thrid commit

--- 3.JavaMultithreading/src/com/javarush/task/tests/TestGit.java
+++ 3.JavaMultithreading/src/com/javarush/task/tests/TestGit.java
@@ -6,5 +6,6 @@ package com.javarush.task.tests;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Test Git");
+        System.out.println("New Line");
     }
 }
